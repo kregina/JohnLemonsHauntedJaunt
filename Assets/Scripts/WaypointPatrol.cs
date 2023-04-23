15148ebac1f90cc9093e6154812446f523b3fe7b
Kelly Silva <kellynha756@gmail.com>
2023-04-23 15:28:11 +0300

    make a prefab ghost, add NavMeshAgent, collider and rigidbody

---
+++ Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointPatrol : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
