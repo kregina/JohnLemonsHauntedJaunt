2a46091e3f5750bb44f4d887bbf41fdc19667322
Kelly Silva <kellynha756@gmail.com>
2023-04-23 15:41:50 +0300

    add waypoint logic

--- Assets/Scripts/WaypointPatrol.cs
+++ Assets/Scripts/WaypointPatrol.cs
@@ -6,13 +6,21 @@ using UnityEngine.AI;
 public class WaypointPatrol : MonoBehaviour
 {
     public NavMeshAgent navMeshAgent;
+    public Transform[] waypoints;
+
+    private int m_CurrentWaypointIndex;
+
     void Start()
     {
-        
+        navMeshAgent.SetDestination(waypoints[0].position);
     }
 
     void Update()
     {
-        
+        if(navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
+            navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
+        }
     }
 }
